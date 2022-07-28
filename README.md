01af00a37e68a93ccb03a130190405bfb138b95b
pafufodon@gmail.com <pafufodon@gmail.com>
2022-07-28 12:04:48 -0300

    PI4: Auto update 0.17 minutos

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 Informações das Ações e dos FII's listados na StatusInvest atualizadas a cada x minutos rodando em um [Raspberry Pi 4 Model B](https://www.raspberrypi.com/) que estava parado.
 
 Atualização automática em: 0.17 minutos. <br>
-Última atualização: 28-07-2022 ás 12:02:37
+Última atualização: 28-07-2022 ás 12:04:28.
 
 
 >Resultados: <br>
--- main.py
+++ main.py
@@ -37,6 +37,31 @@ Exemplo de uso no Googlesheets:
 
 """
 
+def tester():
+    readme_conteudo = f"""
+    # StatusInvest - Dados
+    Informações das Ações e dos FII's listados na StatusInvest atualizadas a cada x minutos rodando em um [Raspberry Pi 4 Model B](https://www.raspberrypi.com/) que estava parado.
+
+    Atualização automática em: {intervalo / 60:.2f} minutos. <br>
+    Última atualização: {last_update}.
+
+
+    >Resultados: <br>
+    [Ações - Googlesheet](https://docs.google.com/spreadsheets/d/e/2PACX-1vS97G13-9owVwSm1y_TAE3gTaxYflhMvgXCYgj3zEGVwqrbPiUrsOyUUcdhM5D7YVJPNaiinn51Plgc/pubhtml?gid=313887204&single=true) <br>
+    [Ações - .csv](https://raw.githubusercontent.com/Antxj/StatusInvestDados/master/resultado/dadosacoes.csv) <br>
+    [FII's - Googlesheet](https://docs.google.com/spreadsheets/d/e/2PACX-1vS97G13-9owVwSm1y_TAE3gTaxYflhMvgXCYgj3zEGVwqrbPiUrsOyUUcdhM5D7YVJPNaiinn51Plgc/pubhtml?gid=1741348998&single=true) <br>
+    [FII's - .csv](https://raw.githubusercontent.com/Antxj/StatusInvestDados/master/resultado/dadosfiis.csv) <br>
+
+
+    Exemplo de uso no Googlesheets:
+    ```sh
+    =IMPORTDATA("https://raw.githubusercontent.com/Antxj/StatusInvestDados/master/resultado/dadosacoes.csv";";";"pt_BR")
+    ```
+
+    ![img_2.png](exemplo.png)
+
+    """
+
 
 # Pastas .git do repositório
 PATH_OF_GIT_REPO_WIN = os.getcwd() + '\.git'
@@ -95,6 +120,7 @@ def atualizar():
     last_update = agora.strftime("%d-%m-%Y ás %H:%M:%S")
 
     # Push
+    tester()
     criar_readme()
     time.sleep(5)
     git_push()
