0496c732a82c11c4ede46bee29fbabfbb1b6f6e9
pafufodon@gmail.com <pafufodon@gmail.com>
2022-07-28 16:57:32 -0300

    PI4: Auto update em: 0.08 minutos

--- main2.py
+++ main2.py
@@ -12,6 +12,32 @@ intervalo = 5  # 14400s = 4h / 7200s = 2h
 agora = datetime.datetime.now()
 last_update = agora.strftime("%d-%m-%Y ás %H:%M:%S")
 
+# README.MD
+readme_conteudo = f"""
+# StatusInvest - Dados
+Informações das Ações e dos FII's listados na StatusInvest atualizadas a cada {intervalo / 60:.2f} minutos rodando em um [Raspberry Pi 4 Model B](https://www.raspberrypi.com/) que estava parado.
+
+Atualização automática em: {intervalo / 60:.2f} minutos. <br>
+<br>Última atualização: {last_update}.  <br>
+
+
+>Resultados: <br>
+[Ações - Googlesheet](https://docs.google.com/spreadsheets/d/e/2PACX-1vS97G13-9owVwSm1y_TAE3gTaxYflhMvgXCYgj3zEGVwqrbPiUrsOyUUcdhM5D7YVJPNaiinn51Plgc/pubhtml?gid=313887204&single=true) <br>
+[Ações - .csv](https://raw.githubusercontent.com/Antxj/StatusInvestDados/master/resultado/dadosacoes.csv) <br>
+[FII's - Googlesheet](https://docs.google.com/spreadsheets/d/e/2PACX-1vS97G13-9owVwSm1y_TAE3gTaxYflhMvgXCYgj3zEGVwqrbPiUrsOyUUcdhM5D7YVJPNaiinn51Plgc/pubhtml?gid=1741348998&single=true) <br>
+[FII's - .csv](https://raw.githubusercontent.com/Antxj/StatusInvestDados/master/resultado/dadosfiis.csv) <br>
+
+
+Exemplo de uso no Googlesheets:
+```sh
+=IMPORTDATA("https://raw.githubusercontent.com/Antxj/StatusInvestDados/master/resultado/dadosacoes.csv";";";"pt_BR")
+```
+
+![img_2.png](exemplo.png)
+
+"""
+
+
 # Pasta .git de acordo com o OS
 my_os = platform.system()  # Windows / Linux
 
@@ -57,30 +83,8 @@ def ler_csv():
 
 # Lendo e salvando os arquivos .csv
 def criar_readme():
-    readme_conteudo = f"""
-    # StatusInvest - Dados
-    Informações das Ações e dos FII's listados na StatusInvest atualizadas a cada {intervalo / 60:.2f} minutos rodando em um [Raspberry Pi 4 Model B](https://www.raspberrypi.com/) que estava parado.
-
-    Atualização automática em: {intervalo / 60:.2f} minutos. <br>
-    <br>Última atualização: {last_update}.  <br>
-
-
-    >Resultados: <br>
-    [Ações - Googlesheet](https://docs.google.com/spreadsheets/d/e/2PACX-1vS97G13-9owVwSm1y_TAE3gTaxYflhMvgXCYgj3zEGVwqrbPiUrsOyUUcdhM5D7YVJPNaiinn51Plgc/pubhtml?gid=313887204&single=true) <br>
-    [Ações - .csv](https://raw.githubusercontent.com/Antxj/StatusInvestDados/master/resultado/dadosacoes.csv) <br>
-    [FII's - Googlesheet](https://docs.google.com/spreadsheets/d/e/2PACX-1vS97G13-9owVwSm1y_TAE3gTaxYflhMvgXCYgj3zEGVwqrbPiUrsOyUUcdhM5D7YVJPNaiinn51Plgc/pubhtml?gid=1741348998&single=true) <br>
-    [FII's - .csv](https://raw.githubusercontent.com/Antxj/StatusInvestDados/master/resultado/dadosfiis.csv) <br>
-
-
-    Exemplo de uso no Googlesheets:
-    ```sh
-    =IMPORTDATA("https://raw.githubusercontent.com/Antxj/StatusInvestDados/master/resultado/dadosacoes.csv";";";"pt_BR")
-    ```
-
-    ![img_2.png](exemplo.png)
-
-    """
-
+    agora = datetime.datetime.now()
+    last_update = agora.strftime("%d-%m-%Y ás %H:%M:%S")
     leitor = open("README.md", "w")  # encoding="cp1252" testar
     leitor.write(readme_conteudo)
     leitor.close()
