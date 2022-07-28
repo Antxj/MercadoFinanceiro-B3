f390be83a3b7ebf8729a848b81e8621496b86988
pafufodon@gmail.com <pafufodon@gmail.com>
2022-07-28 14:53:02 -0300

    PI4: Auto update em: 0.08 minutos

--- main2.py
+++ main2.py
@@ -13,40 +13,29 @@ intervalo = 5  # 14400s = 4h / 7200s = 2h
 agora = datetime.datetime.now()
 last_update = agora.strftime("%d-%m-%Y ás %H:%M:%S")
 
+readme_conteudo = f"""
+# StatusInvest - Dados
+Informações das Ações e dos FII's listados na StatusInvest atualizadas a cada {intervalo / 60:.2f} minutos rodando em um [Raspberry Pi 4 Model B](https://www.raspberrypi.com/) que estava parado.
 
-def f1():
-    readme_conteudo = f"""
-    # StatusInvest - Dados
-    Informações das Ações e dos FII's listados na StatusInvest atualizadas a cada {intervalo / 60:.2f} minutos rodando em um [Raspberry Pi 4 Model B](https://www.raspberrypi.com/) que estava parado.
-
-    Atualização automática em: {intervalo / 60:.2f} minutos. <br>
-    <br>Última atualização: {last_update}.  <br>
+Atualização automática em: {intervalo / 60:.2f} minutos. <br>
+<br>Última atualização: {last_update}.  <br>
 
 
-    >Resultados: <br>
-    [Ações - Googlesheet](https://docs.google.com/spreadsheets/d/e/2PACX-1vS97G13-9owVwSm1y_TAE3gTaxYflhMvgXCYgj3zEGVwqrbPiUrsOyUUcdhM5D7YVJPNaiinn51Plgc/pubhtml?gid=313887204&single=true) <br>
-    [Ações - .csv](https://raw.githubusercontent.com/Antxj/StatusInvestDados/master/resultado/dadosacoes.csv) <br>
-    [FII's - Googlesheet](https://docs.google.com/spreadsheets/d/e/2PACX-1vS97G13-9owVwSm1y_TAE3gTaxYflhMvgXCYgj3zEGVwqrbPiUrsOyUUcdhM5D7YVJPNaiinn51Plgc/pubhtml?gid=1741348998&single=true) <br>
-    [FII's - .csv](https://raw.githubusercontent.com/Antxj/StatusInvestDados/master/resultado/dadosfiis.csv) <br>
+>Resultados: <br>
+[Ações - Googlesheet](https://docs.google.com/spreadsheets/d/e/2PACX-1vS97G13-9owVwSm1y_TAE3gTaxYflhMvgXCYgj3zEGVwqrbPiUrsOyUUcdhM5D7YVJPNaiinn51Plgc/pubhtml?gid=313887204&single=true) <br>
+[Ações - .csv](https://raw.githubusercontent.com/Antxj/StatusInvestDados/master/resultado/dadosacoes.csv) <br>
+[FII's - Googlesheet](https://docs.google.com/spreadsheets/d/e/2PACX-1vS97G13-9owVwSm1y_TAE3gTaxYflhMvgXCYgj3zEGVwqrbPiUrsOyUUcdhM5D7YVJPNaiinn51Plgc/pubhtml?gid=1741348998&single=true) <br>
+[FII's - .csv](https://raw.githubusercontent.com/Antxj/StatusInvestDados/master/resultado/dadosfiis.csv) <br>
 
 
-    Exemplo de uso no Googlesheets:
-    ```sh
-    =IMPORTDATA("https://raw.githubusercontent.com/Antxj/StatusInvestDados/master/resultado/dadosacoes.csv";";";"pt_BR")
-    ```
+Exemplo de uso no Googlesheets:
+```sh
+=IMPORTDATA("https://raw.githubusercontent.com/Antxj/StatusInvestDados/master/resultado/dadosacoes.csv";";";"pt_BR")
+```
 
-    ![img_2.png](exemplo.png)
+![img_2.png](exemplo.png)
 
-    """
-
-    def f2():
-        leitor = open("README.md", "w", encoding="cp1252")  # encoding="cp1252" testar
-        leitor.write(readme_conteudo)
-        leitor.close()
-        print('Readme.md: OK.')
-    f2()
-
-
+"""
 
 # Pasta .git de acordo com o OS
 my_os = platform.system()  # Windows / Linux
@@ -77,6 +66,14 @@ def git_push():
         print('Deu erro na hora do push!')
 
 
+def criar_readme():
+    global readme_conteudo
+    leitor = open("README.md", "w")  # encoding="cp1252" testar
+    leitor.write(readme_conteudo)
+    leitor.close()
+    print('Readme.md: OK.')
+
+
 def atualizar():
     # Baixando o arquivos .csv
     response = requests.get(fontedados.url_acoes)
@@ -93,7 +90,7 @@ def atualizar():
     last_update = agora.strftime("%d-%m-%Y ás %H:%M:%S")
 
     # Readme.md
-    f1()
+    criar_readme()
 
     # Push
     git_push()
