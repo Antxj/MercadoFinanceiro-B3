af196cc013b23e34ef10cc8f060c655fdb71410f
Antxj <antonioxaraj@gmail.com>
2023-02-08 10:47:18 -0300

    v1.1

--- v2/get.py
+++ v2/get.py
@@ -79,7 +79,26 @@ def get_fiis():
     df.to_csv('fiis2.csv', encoding='utf-8', index=False, sep=';', decimal=',')
 
 
+def get_agro():
+
+    session = requests.session()
+    session.headers.update({
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36",
+        "accept": "application/json ..."
+    })
+
+    url = "https://investidor10.com.br/api/fiagro/comparador-mesmo-segmento/table/33/"
+
+    response = session.get(url)
+    data = response.json()["data"]
+
+    df = pd.DataFrame(data)
+    df.to_csv('agro2.csv', encoding='utf-8', index=False, decimal=',')
+
+
 get_acoes()
 get_fiis()
+get_agro()
+
 
 
