2258abd88bec8374d736c57861295798a61b0c3d
Antxj <antonioxaraj@gmail.com>
2023-04-01 12:30:11 -0300

    v1.1

--- dyoc.py
+++ dyoc.py
@@ -1,10 +1,7 @@
-import time
-from fake_useragent import UserAgent
-import os
-import json
-import pandas as pd
 import csv
-import pyautogui
+import os
+
+from fake_useragent import UserAgent
 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
@@ -12,24 +9,16 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
-
-# Pasta de download
-full_path = os.path.realpath(__file__)
-download_folder = (os.path.dirname(full_path))
-# print(f'O arquivo ser· salvo em {download_folder}')
-
-# Caminho Chrome data
-# user_pc = os.getlogin()
-# path_chrome = fr'C:\Users\{user_pc}\AppData\Local\Google\Chrome\User Data\Default'
+from selenium.webdriver.common.keys import Keys
 
 # Chrome
 servico = Service(ChromeDriverManager().install())
 chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument("--headless")  # Headless mode
+# chrome_options.add_argument("--headless")  # Headless mode
 ua = UserAgent(browsers=['chrome'])
 user_agent = ua.random
 # user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'
-# chrome_options.add_argument(rf'--user-data-dir={path_chrome}')  # Funciona mas expira e d· trabalho.
+# chrome_options.add_argument(rf'--user-data-dir={path_chrome}')  # Funciona mas expira e d√° trabalho.
 chrome_options.add_argument(f"user-agent={user_agent}")  # Agent
 chrome_options.add_experimental_option("useAutomationExtension", False)
 chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
@@ -45,8 +34,26 @@ chrome_options.add_argument('--ignore-certificate-errors')
 chrome_options.add_argument("start-maximized")
 chrome_options.add_argument("disable-infobars")
 navegador = webdriver.Chrome(service=servico, options=chrome_options)
-prefs = {f"download.default_directory": f"{download_folder}"}
-chrome_options.add_experimental_option("prefs", prefs)
+
+os.remove('dyoc.csv')
+
+
+def dy_ano_fiis(ticker):
+    url = f'https://statusinvest.com.br/fundos-imobiliarios/{ticker}/'
+    navegador.get(f'{url}')
+    dado = WebDriverWait(navegador, 10).until(EC.visibility_of_element_located(
+        (By.XPATH, '//*[@id="main-2"]/div[2]/div[1]/div[4]/div/div[2]/div/span[2]'))).get_attribute("innerHTML")
+    dado = dado.replace('R$ ', '')
+    print(f'{ticker} - {dado}')
+    with open('dyoc.csv', 'a+', newline='', encoding='utf-8') as csv_file:
+        writer = csv.writer(csv_file, delimiter=',')
+        infos = [f'{ticker}', f'{dado}']
+        writer.writerow(infos)
+
+
+tickers_fiis = ['BCFF11', 'HFOF11']
+for i in tickers_fiis:
+    dy_ano_fiis(i)
 
 
 def dy_ano_acoes(ticker):
@@ -56,6 +63,10 @@ def dy_ano_acoes(ticker):
         (By.XPATH, '//*[@id="main-2"]/div[2]/div/div[1]/div/div[4]/div/div[2]/div/span[2]'))).get_attribute("innerHTML")
     dado = dado.replace('R$ ', '')
     print(f'{ticker} - {dado}')
+    with open('dyoc.csv', 'a+', newline='', encoding='utf-8') as csv_file:
+        writer = csv.writer(csv_file, delimiter=',')
+        infos = [f'{ticker}', f'{dado}']
+        writer.writerow(infos)
 
 
 tickers_acoes = ['ITSA4', 'BBDC4', 'EGIE3', 'AESB3', 'TAEE11', 'TASA4', 'KLBN4', 'SAPR4', 'WIZC3']
